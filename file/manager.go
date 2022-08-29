531698de6bc6aebce9d8aa5f75772955fd6039a6
morita <moritasoushi@gmail.com>
2022-08-29 23:46:21 +0900

    openFiles

--- file/manager.go
+++ file/manager.go
@@ -11,6 +11,7 @@ import (
 type Manager struct {
 	dirname   string
 	blockSize int
+	openFiles map[string]*os.File
 }
 
 func NewManager(dirname string, blockSize int) *Manager {
@@ -36,29 +37,39 @@ func NewManager(dirname string, blockSize int) *Manager {
 	return &Manager{
 		dirname:   dirname,
 		blockSize: blockSize,
+		openFiles: make(map[string]*os.File),
 	}
 }
 
 func (fm *Manager) Write(b *BlockId, page *Page) {
-	filepath := filepath.Join(fm.dirname, b.filename)
+	f := fm.getFile(b.filename)
+	f.Seek(int64(b.blknum*fm.blockSize), io.SeekStart)
+	f.Write(page.Contents())
+}
+
+func (fm *Manager) Read(b *BlockId, page *Page) {
+	f := fm.getFile(b.filename)
+	f.Seek(int64((b.blknum)*fm.blockSize), io.SeekStart)
+	buf := make([]byte, fm.blockSize)
+	f.Read(buf)
+	page.setBytes(0, buf)
+}
+
+// ファイルを取得
+// 存在しなければ作成する
+func (fm *Manager) getFile(filename string) *os.File {
+	f := fm.openFiles[filename]
+	if f != nil {
+		return f
+	}
+
+	filepath := filepath.Join(fm.dirname, filename)
 	f, err := os.OpenFile(filepath, os.O_RDWR, 0755)
 	if os.IsNotExist(err) {
 		if f, err = os.Create(filepath); err != nil {
 			log.Fatal(err)
 		}
 	}
-	f.Seek(int64(b.blknum*fm.blockSize), io.SeekStart)
-	f.Write(page.Contents())
-}
-
-func (fm *Manager) Read(b *BlockId, page *Page) {
-	filepath := filepath.Join(fm.dirname, b.filename)
-	f, err := os.Open(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	f.Seek(int64((b.blknum)*fm.blockSize), io.SeekStart)
-	buf := make([]byte, fm.blockSize)
-	f.Read(buf)
-	page.setBytes(0, buf)
+	fm.openFiles[filename] = f
+	return f
 }
