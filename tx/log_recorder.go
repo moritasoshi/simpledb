7a72d0e087bb127cd27591e54e32bbbd8c6db681
morita <moritasoushi@gmail.com>
2022-09-25 23:02:41 +0900

    unpackage

---
+++ tx/log_recorder.go
@@ -0,0 +1,210 @@
+package tx
+
+import (
+	"github.com/moritasoshi/simpledb/file"
+	"github.com/moritasoshi/simpledb/util"
+)
+
+type LogRecorder interface {
+	Op() int
+	TxNumber() int
+	Undo(tx *Transaction)
+}
+
+const (
+	CHECKPOINT = 1 << iota
+	START
+	COMMIT
+	ROLLBACK
+	SETINT
+	SETSTRING
+)
+
+func CreateLogRecorder(b []byte) LogRecorder {
+	p, err := file.NewPageBytes(b)
+	if err != nil {
+		panic(err)
+	}
+	t, err := p.GetInt(0)
+	if err != nil {
+		panic(err)
+	}
+	switch t {
+	case CHECKPOINT:
+		return NewCheckpoint()
+	case START:
+		return NewStart(p)
+	case COMMIT:
+		return NewCommit(p)
+	case ROLLBACK:
+		return NewRollback(p)
+	case SETINT:
+		return NewSetInt(p)
+	case SETSTRING:
+		return NewSetString(p)
+	default:
+		return nil
+	}
+}
+
+// checkpoint
+type checkpoint struct{}
+
+func NewCheckpoint() LogRecorder           { return &checkpoint{} }
+func (r *checkpoint) Op() int              { return CHECKPOINT }
+func (r *checkpoint) TxNumber() int        { return -1 } // dummy value
+func (r *checkpoint) Undo(tx *Transaction) {}
+
+// commit
+type commit struct {
+	txNum int
+}
+
+func NewCommit(p *file.Page) LogRecorder {
+	tpos := util.INT64_BYTES
+	n, err := p.GetInt(tpos)
+	if err != nil {
+		panic(err)
+	}
+	return &commit{
+		txNum: n,
+	}
+}
+func (r *commit) Op() int              { return COMMIT }
+func (r *commit) TxNumber() int        { return r.txNum }
+func (r *commit) Undo(tx *Transaction) {}
+
+// rollback
+type rollback struct {
+	txNum int
+}
+
+func NewRollback(p *file.Page) LogRecorder {
+	tpos := util.INT64_BYTES
+	n, err := p.GetInt(tpos)
+	if err != nil {
+		panic(err)
+	}
+	return &rollback{
+		txNum: n,
+	}
+}
+func (r *rollback) Op() int              { return ROLLBACK }
+func (r *rollback) TxNumber() int        { return r.txNum }
+func (r *rollback) Undo(tx *Transaction) {}
+
+// start
+type start struct {
+	txNum int
+}
+
+func NewStart(p *file.Page) LogRecorder {
+	tpos := 2 * util.INT64_BYTES
+	txNum, err := p.GetInt(tpos)
+	if err != nil {
+		panic(err)
+	}
+	return &start{txNum: txNum}
+}
+func (r *start) Op() int              { return START }
+func (r *start) TxNumber() int        { return r.txNum }
+func (r *start) Undo(tx *Transaction) {}
+
+// set int
+type setInt struct {
+	txNum, offset, val int
+	blk                *file.BlockId
+}
+
+func NewSetInt(p *file.Page) LogRecorder {
+	tpos := util.INT64_BYTES
+	txNum, err := p.GetInt(tpos)
+	if err != nil {
+		panic(err)
+	}
+	fpos := tpos * util.INT64_BYTES
+	filename, err := p.GetString(fpos)
+	if err != nil {
+		panic(err)
+	}
+	bpos := fpos + file.MaxLength(len(filename))
+	blknum, err := p.GetInt(bpos)
+	if err != nil {
+		panic(err)
+	}
+	blk := file.NewBlockId(filename, blknum)
+	opos := bpos + util.INT64_BYTES
+	offset, err := p.GetInt(opos)
+	if err != nil {
+		panic(err)
+	}
+	vpos := opos + util.INT64_BYTES
+	val, err := p.GetInt(vpos)
+	if err != nil {
+		panic(err)
+	}
+
+	return &setInt{
+		txNum:  txNum,
+		offset: offset,
+		val:    val,
+		blk:    blk,
+	}
+}
+func (r *setInt) Op() int       { return SETINT }
+func (r *setInt) TxNumber() int { return r.txNum }
+func (r *setInt) Undo(tx *Transaction) {
+	tx.Pin(r.blk)
+	tx.SetInt(r.blk, r.offset, r.val, false)
+	tx.Unpin(r.blk)
+}
+
+// set string
+type setString struct {
+	txNum, offset int
+	val           string
+	blk           *file.BlockId
+}
+
+func NewSetString(p *file.Page) LogRecorder {
+	tpos := util.INT64_BYTES
+	txNum, err := p.GetInt(tpos)
+	if err != nil {
+		panic(err)
+	}
+	fpos := tpos * util.INT64_BYTES
+	filename, err := p.GetString(fpos)
+	if err != nil {
+		panic(err)
+	}
+	bpos := fpos + file.MaxLength(len(filename))
+	blknum, err := p.GetInt(bpos)
+	if err != nil {
+		panic(err)
+	}
+	blk := file.NewBlockId(filename, blknum)
+	opos := bpos + util.INT64_BYTES
+	offset, err := p.GetInt(opos)
+	if err != nil {
+		panic(err)
+	}
+	vpos := opos + util.INT64_BYTES
+	val, err := p.GetString(vpos)
+	if err != nil {
+		panic(err)
+	}
+	return &setString{
+		txNum:  txNum,
+		offset: offset,
+		val:    val,
+		blk:    blk,
+	}
+}
+func (r *setString) Op() int       { return SETSTRING }
+func (r *setString) TxNumber() int { return r.txNum }
+func (r *setString) Undo(tx *Transaction) {
+	tx.Pin(r.blk)
+	tx.SetString(r.blk, r.offset, r.val, false)
+	tx.Unpin(r.blk)
+
+}
--- tx/manager.go
+++ tx/manager.go
@@ -1,23 +1,21 @@
-package recovery
+package tx
 
 import (
 	"github.com/moritasoshi/simpledb/buffer"
 	"github.com/moritasoshi/simpledb/file"
 	"github.com/moritasoshi/simpledb/log"
-	"github.com/moritasoshi/simpledb/tx"
-	"github.com/moritasoshi/simpledb/tx/recovery/record"
 	"github.com/moritasoshi/simpledb/util"
 )
 
 type Manager struct {
 	lm    *log.Manager
 	bm    *buffer.Manager
-	tx    *tx.Transaction
+	tx    *Transaction
 	txNum int
 }
 
-func NewManager(tx *tx.Transaction, txNum int, lm *log.Manager, bm *buffer.Manager) *Manager {
-	writeLog(lm, txNum, record.START)
+func NewManager(tx *Transaction, txNum int, lm *log.Manager, bm *buffer.Manager) *Manager {
+	writeLog(lm, txNum, START)
 	return &Manager{
 		tx:    tx,
 		txNum: txNum,
@@ -28,27 +26,27 @@ func NewManager(tx *tx.Transaction, txNum int, lm *log.Manager, bm *buffer.Manag
 
 func (rm *Manager) Commit() {
 	rm.bm.FlushAll(rm.txNum)
-	lsn := writeLog(rm.lm, rm.txNum, record.COMMIT)
+	lsn := writeLog(rm.lm, rm.txNum, COMMIT)
 	rm.lm.Flush(lsn)
 }
 
 func (rm *Manager) Rollback() {
 	rm.doRollback()
 	rm.bm.FlushAll(rm.txNum)
-	lsn := writeLog(rm.lm, rm.txNum, record.ROLLBACK)
+	lsn := writeLog(rm.lm, rm.txNum, ROLLBACK)
 	rm.lm.Flush(lsn)
 }
 
 func (rm *Manager) Recover() {
 	rm.doRecover()
 	rm.bm.FlushAll(rm.txNum)
-	lsn := writeLog(rm.lm, rm.txNum, record.CHECKPOINT)
+	lsn := writeLog(rm.lm, rm.txNum, CHECKPOINT)
 	rm.lm.Flush(lsn)
 }
 
 func writeLog(lm *log.Manager, txNum int, recordType int) int {
 	switch recordType {
-	case record.START, record.COMMIT, record.ROLLBACK:
+	case START, COMMIT, ROLLBACK:
 		rec := make([]byte, 4*util.INT64_BYTES)
 		p, err := file.NewPageBytes(rec)
 		if err != nil {
@@ -57,7 +55,7 @@ func writeLog(lm *log.Manager, txNum int, recordType int) int {
 		p.SetInt(0, recordType)
 		p.SetInt(2*util.INT64_BYTES, txNum)
 		return lm.Append(rec)
-	case record.CHECKPOINT:
+	case CHECKPOINT:
 		rec := make([]byte, 2*util.INT64_BYTES)
 		p, err := file.NewPageBytes(rec)
 		if err != nil {
@@ -74,9 +72,9 @@ func (rm *Manager) doRollback() {
 	iter := rm.lm.Iterator()
 	for iter.HasNext() {
 		b := iter.Next()
-		rec := record.CreateLogRecorder(b)
+		rec := CreateLogRecorder(b)
 		if rec.TxNumber() == rm.txNum {
-			if rec.Op() == record.START {
+			if rec.Op() == START {
 				return
 			}
 			rec.Undo(rm.tx)
@@ -89,11 +87,11 @@ func (rm *Manager) doRecover() {
 	iter := rm.lm.Iterator()
 	for iter.HasNext() {
 		b := iter.Next()
-		rec := record.CreateLogRecorder(b)
-		if rec.Op() == record.CHECKPOINT {
+		rec := CreateLogRecorder(b)
+		if rec.Op() == CHECKPOINT {
 			return
 		}
-		if rec.Op() == record.COMMIT || rec.Op() == record.ROLLBACK {
+		if rec.Op() == COMMIT || rec.Op() == ROLLBACK {
 			finished[rec.TxNumber()] = struct{}{}
 		} else if _, ok := finished[rec.TxNumber()]; ok {
 			rec.Undo(rm.tx)
