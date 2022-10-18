61521feb4653b78f1101561d12230aab948aa497
morita <moritasoushi@gmail.com>
2022-10-19 00:21:46 +0900

    add buffer pool tests

--- buffer/manager_test.go
+++ buffer/manager_test.go
@@ -3,13 +3,14 @@ package buffer_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/moritasoshi/simpledb/buffer"
 	"github.com/moritasoshi/simpledb/file"
 	"github.com/moritasoshi/simpledb/log"
 )
 
-func TestManager(t *testing.T) {
+func _TestManager(t *testing.T) {
 	var err error
 	fm, _ := file.NewManager("buffertest", 400)
 	lm := log.NewManager(fm, "logtest")
@@ -47,5 +48,64 @@ func printBuffers(buffers []*buffer.Buffer) {
 		if b != nil {
 			fmt.Printf("buf[%d] pinned to block %v\n", i, b.Block())
 		}
+	}
+}
+
+// バッファプールへの追加と削除を非同期で実行できること
+func TestPinFailure(t *testing.T) {
+	var err error
+	fm, _ := file.NewManager("buffertest", 400)
+	lm := log.NewManager(fm, "logtest")
+	bm := buffer.NewManager(fm, lm, 3)
+
+	bm.Pin(file.NewBlockId("testfile", 0))
+	bm.Pin(file.NewBlockId("testfile", 1))
+	bm.Pin(file.NewBlockId("testfile", 2))
+
+	// バッファプールに空きがない場合はエラーが返ることを確認
+	_, err = bm.Pin(file.NewBlockId("testfile", 3))
+	if err != buffer.ErrOperationAborted {
+		t.Errorf("want %v, got %v", buffer.ErrOperationAborted, err)
+	}
+}
+
+func TestPinSuccess(t *testing.T) {
+	fm, _ := file.NewManager("buffertest", 400)
+	lm := log.NewManager(fm, "logtest")
+	bm := buffer.NewManager(fm, lm, 2)
+
+	buffers := make([]*buffer.Buffer, 3)
+
+	buffers[0], _ = bm.Pin(file.NewBlockId("testfile", 0))
+	buffers[1], _ = bm.Pin(file.NewBlockId("testfile", 1))
+
+	// バッファプールが利用可能になるとピン留めに成功すること
+	type result struct {
+		buf *buffer.Buffer
+		err error
+	}
+	ch := make(chan result)
+
+	go func() {
+		defer close(ch)
+
+		fmt.Println("Pinning...")
+		buf, e := bm.Pin(file.NewBlockId("testfile", 2))
+		fmt.Println("Pinning finished")
+		ch <- result{buf: buf, err: e}
+	}()
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Unpinning...")
+	bm.Unpin(buffers[1])
+	fmt.Println("Unpinning finished")
+
+	res := <-ch
+	buffers[2] = res.buf
+
+
+	// ピン留めに成功していることを確認
+	if res.err != nil {
+		t.Errorf("want %v, got %v", nil, res.err)
 	}
 }
